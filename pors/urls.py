9221a422319f6c2fb7297d6ee49ebeb16bfec71f
daway0 <rezaee.erfan.2000@gmail.com>
2023-11-10 19:40:26 +0330

    init

---
+++ pors/urls.py
@@ -0,0 +1,13 @@
+
+
+
+
+from django.urls import path, include
+from .views import ui
+
+app_name = "pors"
+
+urlpatterns = [
+    
+    path("", ui)
+]
---
+++ pors/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def ui(request):
+    return render(request, "edari.html")
