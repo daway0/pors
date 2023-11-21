6bf40c15ae22ba58bcbb7b4bafe64eb7ceefc7f2
daway0 <rezaee.erfan.2000@gmail.com>
2023-11-21 18:24:13 +0330

    add id field to this view

--- pors/SQLs/ItemsOrdersPerDay.sql
+++ pors/SQLs/ItemsOrdersPerDay.sql
@@ -1,4 +1,4 @@
-SELECT     dmi.AvailableDate as [Date], dmi.Item_id AS Item, COUNT(oi.id) TotalOrders
+SELECT     ROW_NUMBER() over (ORDER BY dmi.AvailableDate ) as Id,dmi.AvailableDate as [Date], dmi.Item_id AS Item, COUNT(oi.id) TotalOrders
 FROM        dbo.pors_dailymenuitem dmi left JOIN
                   dbo.pors_orderitem oi  ON dmi.AvailableDate = oi.DeliveryDate AND dmi.Item_id = oi.OrderedItem_id
 group by     dmi.AvailableDate, dmi.Item_id
