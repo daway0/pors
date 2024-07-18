976ce57834fc93ceb5f72a56ad1b486340086d2e
ArCaneSec <arcanebughunter@gmail.com>
2024-07-19 02:06:05 +0330

    itemfeedbacks view fixed.

--- pors/SQLs/v_ItemFeedbacks.sql
+++ pors/SQLs/v_ItemFeedbacks.sql
@@ -1,20 +1,25 @@
-select ROW_NUMBER() over (order by (select null)) as id,
-	pi2.id as Item,
-	count(pf.id) as TotalLikes,
-	count(pf2.id) as TotalDissLikes,
-	count(pc.id) as TotalComments
-from
-	pors_item pi2
-left join pors_feedback pf 
-on
-	pf.Item_id = pi2.id
-	and pf.[Type] = 'L'
-left join pors_feedback pf2 
-on
-	pf2.Item_id = pi2.id
-	and pf2.[Type] = 'D'
-left join pors_comment pc 
-on
-	pc.Item_id = pi2.id
-GROUP By
-	pi2.id;
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY pi2.id) AS id,
+    pi2.id AS Item,
+    COALESCE(likes.TotalLikes, 0) AS TotalLikes,
+    COALESCE(dislikes.TotalDislikes, 0) AS TotalDisslikes,
+    COALESCE(comments.TotalComments, 0) AS TotalComments
+FROM 
+    pors_item pi2
+LEFT JOIN (
+    SELECT Item_id, COUNT(*) AS TotalLikes
+    FROM pors_feedback
+    WHERE [Type] = 'L'
+    GROUP BY Item_id
+) likes ON likes.Item_id = pi2.id
+LEFT JOIN (
+    SELECT Item_id, COUNT(*) AS TotalDislikes
+    FROM pors_feedback
+    WHERE [Type] = 'D'
+    GROUP BY Item_id
+) dislikes ON dislikes.Item_id = pi2.id
+LEFT JOIN (
+    SELECT Item_id, COUNT(*) AS TotalComments
+    FROM pors_comment
+    GROUP BY Item_id
+) comments ON comments.Item_id = pi2.id;
